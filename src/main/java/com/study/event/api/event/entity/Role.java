50277b9b1e145f8f5b38b74a66e7dae448e03c41
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-11 14:15:50 +0900

    ✅ 001. EventUser, EmailVerification Entity & db 추가

---
+++ src/main/java/com/study/event/api/event/entity/Role.java
@@ -0,0 +1,5 @@
+package com.study.event.api.event.entity;
+
+public enum Role {
+    COMMON, PREMIUM, ADMIN // 회원 권한은 일반, 유료, 관리자
+}
--- src/main/resources/static/db/EVENT_API20240710.sql
+++ src/main/resources/static/db/EVENT_API20240710.sql
@@ -3,3 +3,9 @@
 -- Event 테이블 전체조회
 select * from tbl_event;
 
+-- Event_USER 테이블 전체조회
+select * from tbl_event_user;
+
+-- Event_EMAIL_VERIFICATION 테이블 전체조회
+select * from tbl_email_verification;
+
