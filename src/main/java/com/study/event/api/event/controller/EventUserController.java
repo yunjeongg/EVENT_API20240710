c1085c70ca65166a3b9e42be4f78b84d43f538d2
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-11 14:44:41 +0900

    ✅ 004. 인증 이메일 발송하기 01

--- src/main/java/com/study/event/api/event/controller/EventUserController.java
+++ src/main/java/com/study/event/api/event/controller/EventUserController.java
@@ -21,11 +21,17 @@ public class EventUserController {
     public ResponseEntity<?> checkEmail(String email) {
         boolean isDuplicate = eventUserService.checkEmailDuplicate(email);
 
+        // 중복된 이메일이 아니면 인증코드메일 발송
+        if (!isDuplicate) {
+            eventUserService.sendVerificationEmail(email);
+        }
+
         return ResponseEntity.ok().body(isDuplicate);
 
         // postman
         // get, http://localhost:8787/auth/check-email?email=aaa@gmail.com
         // EventUserService 에서 등록 요청한 이메일이 DB 에 없을 경우 false, 있으면 true
+        // aaa@gmail.com 에 application.yml에서 작성한 이메일로부터 인증코드 메일 가는걸 확인할 수 있다.
     }
 
 
--- src/main/java/com/study/event/api/event/service/EventUserService.java
+++ src/main/java/com/study/event/api/event/service/EventUserService.java
@@ -4,8 +4,13 @@ import com.study.event.api.event.entity.EventUser;
 import com.study.event.api.event.repository.EventUserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.mail.internet.MimeMessage;
 
 @Service
 @Slf4j
@@ -14,18 +19,63 @@ import org.springframework.transaction.annotation.Transactional;
 public class EventUserService {
 
     private final EventUserRepository eventUserRepository;
+    
+    @Value("${study.mail.host}") // SpringFramework 의 @Value 로 가져오기
+    private String mailHost;
+
+    // 이메일 전송 객체
+    private final JavaMailSender mailSender;
 
     // 이메일 중복확인 처리
     public boolean checkEmailDuplicate(String email) {
 
-        EventUser user = EventUser.builder()
-                .email("abc@def.com" + (int) (Math.random() * 10))
-                .build();
-        eventUserRepository.save(user);
+//        EventUser user = EventUser.builder()
+//                .email("abc@def.com" + (int) (Math.random() * 10))
+//                .build();
+//        eventUserRepository.save(user);
 
         boolean exists = eventUserRepository.existsByEmail(email);
         log.info("Checking email {} is duplicate : {}", email, exists);
         return exists;
     }
 
+    // 이메일 인증 코드 보내기
+    public void sendVerificationEmail(String email) {
+
+        // 검증 코드 생성하기
+        String code = generateVerificationCode();
+
+        // 이메일을 전송할 객체 생성
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+
+        try {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+
+            // 누구에게 이메일을 보낼 것인지
+            messageHelper.setTo(email);
+            // 이메일 제목 설정
+            messageHelper.setSubject("[인증메일] 중앙정보스터디 가입 인증 메일입니다.");
+            // 이메일 내용 설정
+            messageHelper.setText(
+                    "인증 코드: <b style=\"font-weight: 700; letter-spacing: 5px; font-size: 30px;\">" + code + "</b>"
+                    , true
+            );
+
+            // 전송자의 이메일 주소
+            messageHelper.setFrom(mailHost);
+
+            // 이메일 보내기
+            mailSender.send(mimeMessage);
+
+            log.info("{} 님에게 이메일 전송!", email);
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // 검증 코드 생성 로직 1000~9999 사이의 4자리 숫자
+    private String generateVerificationCode() {
+        return String.valueOf((int) (Math.random() * 9000 + 1000));
+    }
 }
