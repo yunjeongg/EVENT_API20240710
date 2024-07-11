fbc2fa49b367b6b78784b38624c458e44b1b3372
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-11 14:11:50 +0900

    📝 000. 기존 Event 가져오기

---
+++ src/main/java/com/study/event/api/event/repository/EventRepositoryCustom.java
@@ -0,0 +1,16 @@
+package com.study.event.api.event.repository;
+
+import com.study.event.api.event.entity.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+public interface EventRepositoryCustom {
+
+    Page<Event> findEvents(Pageable pageable, String sort);
+
+    // ...
+
+    // ...
+
+    // ...
+}
---
+++ src/main/resources/static/db/EVENT_API20240710.sql
@@ -0,0 +1,5 @@
+-- Event_Api20240710
+
+-- Event 테이블 전체조회
+select * from tbl_event;
+
