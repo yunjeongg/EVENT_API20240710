d17b20b66994f4f33bcd33a718a92df400893c56
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-11 10:45:13 +0900

    📝 000. initial commit

---
+++ src/test/java/com/study/event/api/EventApi20240710ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.event.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventApi20240710ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
