978183b0ca5015ddcfc1a82669cfa42db9cb29d2
yunjeongg <bae.yunjeongg@gmail.com>
2024-07-15 11:05:24 +0900

    ✅ 015. 로그인 성공 후 인증정보 관리 03 스프링 시큐리티 인증인가 설정

--- src/main/java/com/study/event/api/config/SecurityConfig.java
+++ src/main/java/com/study/event/api/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.study.event.api.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -31,9 +33,19 @@ public class SecurityConfig {
                 .csrf().disable() // 필터설정 off
                 .httpBasic().disable() // 베이직 인증 off
                 .formLogin().disable() // 로그인창 off
+                // 토큰인증을 사용할 것이기 때문에 세션 인증은 더 이상 사용하지 않음 처리 (session off)
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
                 .authorizeRequests() // 요청 별로 인가 설정
-                .antMatchers("/**").permitAll() // 인가 설정 off
-        ;
+
+                // 아래의 URL 요청은 모두 허용
+                .antMatchers("/", "/auth/**").permitAll() // "/auth/**" -- 모든 사용자가 로그인, 중복확인 등등 접근 가능
+                // .antMatchers(HttpMethod.POST, "/events/**").hasAnyRole("VIP", "ADMIN") // 특정 권한만 접근가능
+                
+                // 나머지 요청은 전부 인증(로그인) 후 진행해라
+                // .anyRequest().permitAll() // 인가 설정 off, 로그인 안해도 글쓰기 등 기능 사용 가능
+                .anyRequest().authenticated(); // 인가 설정 on
 
         return http.build();
     }
